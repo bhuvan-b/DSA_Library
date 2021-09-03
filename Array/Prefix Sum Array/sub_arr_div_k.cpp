d54a26e45ae5c8d9fc5370b3603a6eeb577e718a
bhuvan-b <bokkabhuvan2@gmail.com>
2021-09-03 23:54:54 +0530

    Create sub_arr_div_k.cpp

---
+++ Array/Prefix Sum Array/sub_arr_div_k.cpp
@@ -0,0 +1,4 @@
+Problem: Count all sub - arrays having sum divisible by k
+Problem Statement Link: https://www.codingninjas.com/codestudio/guided-paths/data-structures-algorithms/content/118820/offering/1381867
+
+Solution Approach:
